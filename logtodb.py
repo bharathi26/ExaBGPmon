5f740b1ff8df6e490510bacc428fbafde7abc9d4
Mat Wood <mat@thepacketgeek.com>
2015-06-20 18:54:40 -0700

    Adding .current_prefixes to peer object
    Used to track currently advertised prefixes by peers. Modified during
    each announcement or withdraw update message.

--- logtodb.py
+++ logtodb.py
@@ -7,143 +7,154 @@ from datetime import datetime
 import time
 from pymongo import MongoClient
 
-#### Syslog
+########################################
+###### Syslog for Troubleshooting ######
+########################################
 import syslog
 
 def _prefixed (level, message):
-	now = time.strftime('%a, %d %b %Y %H:%M:%S',time.localtime())
-	return "%s %-8s %-6d %s" % (now,level,os.getpid(),message)
+    now = time.strftime('%a, %d %b %Y %H:%M:%S',time.localtime())
+    return "%s %-8s %-6d %s" % (now,level,os.getpid(),message)
 
 syslog.openlog("ExaBGP")
 
-
-
+########################################
+##############  DB Setup ###############
+########################################
 client = MongoClient()
 db = client.exabgp_db
 updates = db.bgp_updates
 bgp_peers = db.bgp_peers
 
-# When the parent dies we are seeing continual newlines, so we only access so many before stopping
 counter = 0
 
 def object_formatter(line):
-	temp_message = json.loads(line)
+    temp_message = json.loads(line)
 
-	try:
-		timestamp = datetime.fromtimestamp(temp_message['time'])
-	except Exception as e:
-		syslog.syslog(syslog.LOG_ALERT, _prefixed('ERROR', 'Peer: %s, %s' % (temp_message['neighbor']['ip'], e.message)))
-		return None
+    try:
+        timestamp = datetime.fromtimestamp(temp_message['time'])
+    except Exception as e:
+        syslog.syslog(syslog.LOG_ALERT, _prefixed('ERROR', 'Peer: %s, %s' % (temp_message['neighbor']['ip'], e.message)))
+        return None
 
-	if temp_message['type'] == 'update':
-		
-		#Ignore EoR updates
-		if temp_message['neighbor']['message'].get("eor", None):
-			# syslog.syslog(syslog.LOG_ALERT, _prefixed('INFO', 'Peer: %s, Update EoR' % temp_message['neighbor']['ip']))
-			return None
+    if temp_message['type'] == 'update':
+        
+        #Ignore EoR updates
+        if temp_message['neighbor']['message'].get("eor", None):
+            # syslog.syslog(syslog.LOG_ALERT, _prefixed('INFO', 'Peer: %s, Update EoR' % temp_message['neighbor']['ip']))
+            return None
 
-		try:
-			message = {
-				'type': 'update',
-				'time': timestamp,
-				'peer': temp_message['neighbor']['ip'],
-			}
-			
-			# Get rid of IP address keys, use list prefixes instead
-			update = {}
-			for section, info in temp_message['neighbor']['message']['update'].iteritems():
-			    if section == 'attribute':
-			        update['attribute'] = info
-			    elif section == 'announce':
-			        announce = {}
-			        for family, peer in info.iteritems():
-			            prefix_list = []
-			            for peer_adv in peer.values():
-			                for prefix in peer_adv:
-			                    prefix_list.append(prefix)
-			        announce[family] = prefix_list
-			        update['announce'] = announce
-			    elif section == 'withdraw':
-			        withdraw = {}
-			        for family, peer in info.iteritems():
-			            prefix_list = []
-			            for prefix, value in peer.iteritems():
-			                prefix_list.append(prefix)
-			        withdraw[family] = prefix_list
-			        update['withdraw'] = withdraw
+        try:
+            message = {
+                'type': 'update',
+                'time': timestamp,
+                'peer': temp_message['neighbor']['ip'],
+            }
+            
+            # Get rid of IP address keys, use list prefixes instead
+            update = {}
+            for section, info in temp_message['neighbor']['message']['update'].iteritems():
+                if section == 'attribute':
+                    update['attribute'] = info
 
-			message['update'] = update
+                elif section == 'announce':
+                    announce = {}
+                    for family, peer in info.iteritems():
+                        prefix_list = []
+                        for peer_adv in peer.values():
+                            for prefix in peer_adv:
+                                # Add to peer list of current prefixes
+                                bgp_peers.update_one({'ip': message['peer']}, {'$addToSet': {'current_prefixes': prefix}})
+                                
+                                prefix_list.append(prefix)
+                    announce[family] = prefix_list
+                    update['announce'] = announce
+
+                elif section == 'withdraw':
+                    syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', temp_message))
+                    withdraw = {}
+                    for family, peer in info.iteritems():
+                        prefix_list = []
+                        for prefix in peer:
+                            # Remove from peer list of current prefixes
+                            bgp_peers.update_one({'ip': message['peer']}, {'$pull': {'current_prefixes': prefix}})
+
+                            prefix_list.append(prefix)
+                    withdraw[family] = prefix_list
+                    update['withdraw'] = withdraw
+
+            message['update'] = update
 
 
-			return message
+            return message
 
-		except KeyError:
-			syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', line))
+        except KeyError:
+            syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', line))
 
 
 
-	if temp_message['type'] == 'keepalive':
-		message = {
-			'type': 'keepalive',
-			'time': timestamp,
-			'peer': temp_message['neighbor']['ip'],
-		}
+    if temp_message['type'] == 'keepalive':
+        message = {
+            'type': 'keepalive',
+            'time': timestamp,
+            'peer': temp_message['neighbor']['ip'],
+        }
 
-		return message
+        return message
 
-	if temp_message['type'] == 'state':
-		message = {
-			'type': 'state',
-			'time': timestamp,
-			'peer': temp_message['neighbor']['ip'],
-			'state': temp_message['neighbor']['state'],
-		}
+    if temp_message['type'] == 'state':
+        message = {
+            'type': 'state',
+            'time': timestamp,
+            'peer': temp_message['neighbor']['ip'],
+            'state': temp_message['neighbor']['state'],
+        }
 
-		bgp_peers.update_one({'ip': message['peer']}, {'$set': {'state': message['state']}})
+        bgp_peers.update_one({'ip': message['peer']}, {'$set': {'state': message['state']}})
 
-		if message['state'] in ('up', 'connected'):
-			return message
+        if message['state'] in ('up', 'connected'):
+            return message
 
-		try:
-			if 'closed by the remote end' in temp_message['neighbor']['reason']:
-				message['reason'] = 'Peer closed connection'
-			else:
-				message['reason'] = temp_message['neighbor']['reason']
-		except KeyError:
-			syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', temp_message))
+        try:
+            if 'closed by the remote end' in temp_message['neighbor']['reason']:
+                message['reason'] = 'Peer closed connection'
+            else:
+                message['reason'] = temp_message['neighbor']['reason']
+        except KeyError:
+            syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', temp_message))
 
-		return message
+        return message
 
-	else:
-		return None
+    else:
+        return None
 
 while True:
-	try:
-		line = sys.stdin.readline().strip()
-		if line == "":
-			counter += 1
-			if counter > 100:
-				break
-			continue
+    try:
+        line = sys.stdin.readline().strip()
+        
+        # When the parent dies we are seeing continual newlines, so we only access so many before stopping
+        if line == "":
+            counter += 1
+            if counter > 100:
+                break
+            continue
 
-		counter = 0
-		
-		message = object_formatter(line)
+        counter = 0
+        
+        message = object_formatter(line)
 
-		if message:
-			syslog.syslog(syslog.LOG_ALERT, _prefixed('INFO', message))
-			try:
-				updates.insert_one(message)
-			except:
-				syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', message))
-		# else:
-		# 	syslog.syslog(syslog.LOG_ALERT, _prefixed('WARNING', line))
+        if message:
+            syslog.syslog(syslog.LOG_ALERT, _prefixed('INFO', message))
+            try:
+                updates.insert_one(message)
+            except:
+                syslog.syslog(syslog.LOG_ALERT, _prefixed('DEBUG', message))
 
-	except KeyboardInterrupt:
-		pass
-	except IOError:
-		# most likely a signal during readline
-		pass
+    except KeyboardInterrupt:
+        pass
+    except IOError:
+        # most likely a signal during readline
+        pass
 
 client.close()
 
