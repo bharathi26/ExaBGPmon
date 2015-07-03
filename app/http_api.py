17580108f0ee14bbee92da611151e227a645c10b
Mat Wood <mat@thepacketgeek.com>
2015-07-02 19:54:23 -0700

    Switching to non-flask HTTP server for ExaBGP
    Should remove intermittent issues with Flask threaded server not
    quitting properly

--- app/http_api.py
+++ app/http_api.py
@@ -10,20 +10,36 @@ reading. Does no sort of validation or response checking from ExaBGP.
 
 """
 
-from flask import Flask, request
+import cgi
+import SimpleHTTPServer
+import SocketServer
 from sys import stdout
  
-app = Flask(__name__)
+PORT = 5001
  
-# Setup a command route to listen for prefix advertisements
-@app.route('/', methods=['POST'])
-def command():
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
  
-    command = request.form['command']
-    stdout.write('%s\n' % command)
-    stdout.flush()
-     
-    return 'Success: %s\n' % command
+    def createResponse(self, command):
+        """ Send command string back as confirmation """
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/text')
+        self.end_headers()
+        self.wfile.write(command)
+        self.wfile.close()
  
-if __name__ == '__main__':
-    app.run(port=5001, debug=False)
+    def do_POST(self):
+        """ Process command from POST and output to STDOUT """
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST'})
+        command = form.getvalue('command')
+        stdout.write('%s\n' % command)
+        stdout.flush()
+        self.createResponse('Success: %s' % command)
+ 
+handler = ServerHandler
+httpd = SocketServer.TCPServer(('', PORT), handler)
+stdout.write('serving at port %s\n' % PORT)
+stdout.flush()
+httpd.serve_forever()
